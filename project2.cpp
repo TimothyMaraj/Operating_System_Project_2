32f491cfe7ac67d313d9725af3821071451b2cbf
TimothyMaraj <timxmaraj@gmail.com>
2023-04-08 23:38:23 -0500

    Initial Commit

---
+++ project2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <pthread.h>
+
+
+
+void* print_char(void* arg) {
+    char c = *(char*)arg;
+    std::cout << c << std::endl;
+    return NULL;
+}
+
+
+int main()
+{
+    std::string str = "hello world!";
+    int n = str.length();
+
+    pthread_t threads[n];
+    for (int i = 0; i < n; i++) {
+        pthread_create(&threads[i], NULL, print_char, &str[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
